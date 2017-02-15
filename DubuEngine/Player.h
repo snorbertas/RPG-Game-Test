7788a56d6df0f14228cd957558a38f85e417a7eb
Norbert Staskevicius <snorbertas@gmail.com>
2017-02-15 19:57:02 +1300

    Fixed digging animation

--- DubuEngine/Player.h
+++ DubuEngine/Player.h
@@ -17,7 +17,7 @@ public:
 		FrameIdleDown_0 = 0, FrameIdleDown_1 = 9,
 		FrameIdleHor_0 = 4, FrameIdleHor_1 = 10,
 		FrameIdleUp_0 = 7, FrameIdleUp_1 = 11,
-		FrameDig_0 = 11, FrameDig_1,
+		FrameDig_0 = 11, FrameDig_1 = 13,
 		FramePee = 12,
 		FrameDrinkUp = 13, FrameDrinkDown = 14, FrameDrinkHor = 15
 	};
